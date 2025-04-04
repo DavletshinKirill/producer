c4fe1cfbd112d4a20cb81916fac61e87efed4103
kirill <kirill.davletshin.02@yandex.ru>
2025-04-04 16:14:03 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+
+import uvicorn
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+
+from car.car import NumberTrafficLightValidation
+from car.car_factory import create_cars_for_minute, create_five_cars_by_traffic_light
+from producer import connect_with_rabbit_mq
+from traffic_light.traffic_light_factory import start_traffic_light
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+   asyncio.create_task(start_traffic_light())
+   connect_with_rabbit_mq()
+
+@app.post("/enable_creating_cars")
+async def root():
+    asyncio.create_task(create_cars_for_minute())
+    return JSONResponse(status_code=200, content={"message": "Success"})
+
+@app.post("/create_traffic_light")
+async def create_five_cars(number_traffic_light: NumberTrafficLightValidation):
+    await create_five_cars_by_traffic_light(number_traffic_light.number_traffic_light)
+    return JSONResponse(status_code=200, content={"message": "Success"})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
